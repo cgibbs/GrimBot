8604a73e6922fa5b96bbc6cac23ef111c0d10257
cgibbs <kcg005@shsu.edu>
2019-07-07 16:59:56 -0500

    GrimBot rides again

--- grimbot/ShouldWeDoThisDumbThing.js
+++ grimbot/ShouldWeDoThisDumbThing.js
@@ -1,7 +1,8 @@
 var env = require('../config.json');
 var fs = require('fs');
 var _ = require('lodash');
-var Dice = require('node-dice-js');
+//var Dice = require('node-dice-js');
+var dice = require('droll');
 
 var filename = './grimbot/currentStuff.json';
 
@@ -9,12 +10,13 @@ var SWDTDTModule = function () {};
 
 SWDTDTModule.prototype.Message = function(keyword, message, callback)
 {
-	var dice = new Dice();
+//	var dice = new Dice();
 	var mes = '1d10';
 	var results = {};
 
-	results = dice.execute(mes);
-	if (results.outcomes[0].total > 9) return callback('Yes, we should do this dumb thing.');
+//	results = dice.execute(mes);
+	results = dice.validate(mes);
+	if (results.total > 9) return callback('Yes, we should do this dumb thing.');
     return callback('No, we should not do this dumb thing.');
 }
 
--- index.js
+++ index.js
@@ -6,18 +6,19 @@ var ins = new InsomBot;
 var discordjs = new Discord.Client();
 
 discordjs.on("ready", function () {
-    console.log("Ready to begin! Serving in " + discordjs.channels.length + " channels");
+    console.log("Ready to begin! Serving in " + JSON.stringify(discordjs.channels) + " channels");
 });
 
 discordjs.on('message', function(msg)
 {
+    console.log('msg received');
     if (typeof ins.loadKeywords() !== 'undefined' && ins.loadKeywords().length > 0) {
         ins.checkMessageForKeywords(msg.content, ins.loadKeywords(), function(keyword)
         {
             if (keyword != 0) {
                 ins.runKeywordFunction(ins.getKeyByValue(ins.keywords, keyword), keyword, msg, function(reply)
                 {
-                    discordjs.reply(msg, reply);
+                    msg.reply(reply);
                 });
             }
         });
@@ -29,4 +30,4 @@ discordjs.on('disconnected', function () {
     process.exit(1);
 });
 
-discordjs.login(env.discord.email, env.discord.password);
+discordjs.login(env.discord.token);
