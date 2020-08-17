a939b61823e4ad422b70d5f4d752319820b1ebed
NuJones <kcg005@shsu.edu>
2020-08-16 19:20:21 -0500

    whoops

--- grimbot/ForgiveMePadre.js
+++ grimbot/ForgiveMePadre.js
@@ -8,6 +8,7 @@ var ForgiveMePadreModule = function () {};
 ForgiveMePadreModule.prototype.Message = function(keyword, message, callback)
 {
 	try {
+		callbackText = "";
 	    var jsonObject = JSON.parse(fs.readFileSync(filename));
 
 	    var newEntry = {};
@@ -16,17 +17,17 @@ ForgiveMePadreModule.prototype.Message = function(keyword, message, callback)
 
 	    var oldEntry = jsonObject[message.author.username];
 
-	    jsonObject[message.author.username] = newEntry;
-
-	    fs.writeFile(filename, JSON.stringify(jsonObject), (err) => { if (err) throw err; });
-
-	    callbackText = 'You last succumbed to GAS on ' + oldEntry.whenBoughtThing 
-	    	+ ', when you purchased the following: ' + oldEntry.boughtThing 
-	    	+ '. I shall remember this new infraction, until next we meet.';
 	    if (!oldEntry) {
 	    	callbackText = 'This is your first confession. I shall remember this infraction, until next we meet.';
-	    }
+	    } else {
+		    jsonObject[message.author.username] = newEntry;
 
+		    fs.writeFile(filename, JSON.stringify(jsonObject), (err) => { if (err) throw err; });
+
+		    callbackText = 'You last succumbed to GAS on ' + oldEntry.whenBoughtThing 
+		    	+ ', when you purchased the following: ' + oldEntry.boughtThing 
+		    	+ '. I shall remember this new infraction, until next we meet.';
+		}
 	    return callback(callbackText);
 	} catch (e) { // forgive me, a different padre
 		console.log(e);
