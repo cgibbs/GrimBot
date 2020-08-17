38d8ccd940a17447f2c039baa1c25d49bb64b449
NuJones <kcg005@shsu.edu>
2020-08-16 19:44:17 -0500

    double whoops

--- grimbot/ForgiveMePadre.js
+++ grimbot/ForgiveMePadre.js
@@ -10,6 +10,7 @@ ForgiveMePadreModule.prototype.Message = function(keyword, message, callback)
 	try {
 		callbackText = "";
 	    var jsonObject = JSON.parse(fs.readFileSync(filename));
+	    console.log(jsonObject);
 
 	    var newEntry = {};
 	    newEntry.boughtThing = message.content.split(" ").slice(1).join(" ");
@@ -20,14 +21,16 @@ ForgiveMePadreModule.prototype.Message = function(keyword, message, callback)
 	    if (!oldEntry) {
 	    	callbackText = 'This is your first confession. I shall remember this infraction, until next we meet.';
 	    } else {
-		    jsonObject[message.author.username] = newEntry;
-
-		    fs.writeFile(filename, JSON.stringify(jsonObject), (err) => { if (err) throw err; });
-
 		    callbackText = 'You last succumbed to GAS on ' + oldEntry.whenBoughtThing 
 		    	+ ', when you purchased the following: ' + oldEntry.boughtThing 
 		    	+ '. I shall remember this new infraction, until next we meet.';
 		}
+
+	    jsonObject[message.author.username] = newEntry;
+	    console.log(jsonObject);
+	    fs.writeFile(filename, JSON.stringify(jsonObject), (err) => { if (err) throw err; });
+
+
 	    return callback(callbackText);
 	} catch (e) { // forgive me, a different padre
 		console.log(e);
